ed5ba59a1ec1417997c24f070988039bed9bd18b
lucaszinn1 <lucas.zinn@vaynermedia.com>
2025-08-22 16:21:35 -0400

    Fix CoffeeShopCard import error

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -5,7 +5,7 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { CoffeeShopCard } from '@/components/CoffeeShopCard';
+import CoffeeShopCard from '@/components/CoffeeShopCard';
 
 const SearchPage = () => {
   const [searchQuery, setSearchQuery] = useState("");
